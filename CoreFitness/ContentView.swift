4c171406063c4137e3873209d77180c80ebc5327
nxkzm <kzmsw0421@gmail.com>
2025-04-26 17:23:32 +0900

    initial commit

---
+++ CoreFitness/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RootScreen: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    RootScreen()
+}
---
+++ CoreFitness/CoreFitnessApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct CoreFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootScreen()
+        }
+    }
+}
